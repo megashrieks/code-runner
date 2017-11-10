686ec54a1fdd1e5e3b8f6a79f76f83d8a3e62e2c
= <shrikanthbudya@gmail.com>
2017-11-10 16:16:24 +0530

    created the temp folder if it doesnt exist to store the executable file.

--- modules/code-executor.js
+++ modules/code-executor.js
@@ -1,7 +1,7 @@
 var python = require('./languages/python');
 var gpp = require('./languages/gpp');
 var fs = require('fs');
-
+var path = require('path');
 
 module.exports = function(code,options,cb){
     //define the extension and their compilers
@@ -32,18 +32,21 @@ module.exports = function(code,options,cb){
     //file name for the code file
     var file_name = random_string();
     //create a file with code on it
+    fs.mkdir('./temp/',function(err){
+        // if(err) throw err;
         fs.writeFile('./temp/' + file_name + extension, code, function (err) {
             if (err) throw err;
             //execute the file with the respective compiler
-            var output = compiler(file_name, options,extension);
+            var output = compiler(file_name, options, extension);
             //delete the file after it is used
             output.then(function (out) {
                 fs.unlink('./temp/' + file_name + extension, function (err) {
                     if (err) throw err;
                 });
-                cb(null,out);
+                cb(null, out);
             });
         });
+    });
 }
 
 function random_string(post_str) {
