2391d2b456150662147a8b7b0f45fbbd2496173e
acnologia <shrikanthbudya@gmail.com>
2017-12-11 20:23:23 +0530

    minor changes

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 var runner = require("./modules/code-runner");
-runner(
-  `#include <stdio.h>
-  int main(){
-    char s[5];
-    while(true);
-    return 1;
-  }`,
-  {
-    language: "C",
-    timeout:100
-  },
-  function(err, data) {
-    if (err) throw err;
-    console.log(data);
-  }
-);
+// runner(
+//   `#include <stdio.h>
+//   int main(){
+//     char s[5];
+//     while(true);
+//     return 1;
+//   }`,
+//   {
+//     language: "C",
+//     timeout:100
+//   },
+//   function(err, data) {
+//     if (err) throw err;
+//     console.log(data);
+//   }
+// );
+exports["code-runner"] = runner;
